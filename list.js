c2eb4681bc35c2902be1f9c1f539b56d1cb240e8
Janette Campbell <campbjp@hotmail.com>
2022-08-01 10:45:02 -0400

    chore: changed file tor read file

--- list.js
+++ list.js
@@ -1,10 +1,7 @@
 const fs = require("fs");
 
-let files = fs.readdir("./", function (err, files) {
-  if (err) {
-    throw err;
-  }
-  console.log(files);
+fs.readFile("./readme.md", "UTF-8", (err, ipsum) => {
+  console.log(ipsum);
 });
 
-console.log("reading files ...");
+console.log("reading the file...");
